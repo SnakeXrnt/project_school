aa3b858e65523a3721da12e54ad93fc0e96c3474
SnakeXrnt <ethanbastian11@gmail.com>
2020-10-26 16:46:12 +0700

    form registrasi

---
+++ settings.py
@@ -0,0 +1,9 @@
+
+class Settings:
+
+    def __init__(self):
+        self.title = 'form perndaftaran siswa baru'
+        self.font = 'arial'
+        self.font_title_size = 20
+        self.font_cont_size = 16
+        self.allign = 'center'
