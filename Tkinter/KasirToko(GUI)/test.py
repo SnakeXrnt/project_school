77766a8b95a611c7f1031d64eaa5ac5b3b25e4f9
SnakeXrnt <ethanbastian11@gmail.com>
2020-12-21 21:51:26 +0700

    Tkinter row2 buat list harga , buat login info realtime sdh selesai

--- Tkinter/KasirToko(GUI)/test.py
+++ Tkinter/KasirToko(GUI)/test.py
@@ -1,13 +1,39 @@
 from json import *
+from datetime import  datetime
+from reportlab.pdfgen import canvas
+from pandas import *
 
-f = open('barang.json','r+')
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
 
-data = {
+now = datetime.now()
+
+q = str(now) +  ' LIST HARGA DAN STOCK BARANG.pdf'
+
+styles = getSampleStyleSheet()
+report = SimpleDocTemplate('report/LIST HARGA DAN STOCK BARANG.pdf')
+report_title = Paragraph("LIST HARGA DAN STOCK BARANG", styles["h1"])
+report.title = (str(now))
+report.build([report_title])
+
+fruit = {
 
 }
 
-data = load(f)
+f = open('login_info.json','r')
 
-v = input('CODE : ')
+fruit = load(f)
 
-print(data[v]['jumlah'])
+table_data = []
+for k, v in fruit.items():
+    table_data.append([k, v])
+
+print(table_data)
+report_table = Table(data=table_data)
+report.build([report_title, report_table])
+from reportlab.lib import colors
+
+table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
+report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
+report.build([report_title, report_table])
